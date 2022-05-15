b3ab75ab90d3eac3bfa01b57823f2d7cc8d721b1
kozlov211 <sayf1ck64.7@gmail.com>
2022-05-15 10:30:05 +0300

    error

---
+++ lesson22.9/request_queue.cpp
@@ -0,0 +1,40 @@
+#pragma once
+#include "request_queue.h"
+
+using namespace std;
+
+RequestQueue::RequestQueue(const SearchServer& search_server) : search_server_ (search_server) {};
+
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
+    ++real_time;
+    vector<Document> result = search_server_.FindTopDocuments(raw_query, status);
+    if (real_time > min_in_day_) {
+        count_empty_request_ -= requests_.front().type;
+	requests_.pop_front();
+	requests_.push_back({result, result.empty()});
+	count_empty_request_ += requests_.back().type;
+    } else {
+	requests_.push_back({result, result.empty()});
+	count_empty_request_ += requests_.back().type;
+    }
+    return result;
+}
+
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
+    ++real_time;
+    vector<Document> result = search_server_.FindTopDocuments(raw_query);
+    if (real_time > min_in_day_) {
+	count_empty_request_ -= requests_.front().type;
+	requests_.pop_front();
+	requests_.push_back({result, result.empty()});
+	count_empty_request_ += requests_.back().type;
+    } else {
+	requests_.push_back({result, result.empty()});
+	count_empty_request_ += requests_.back().type;
+    }
+    return result;
+}
+
+int RequestQueue::GetNoResultRequests() const {
+    return count_empty_request_;
+}
